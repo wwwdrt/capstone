ee836a987f3dbf411a0917835874b27c8e068004
juandapps <juandapps@hotmail.com>
2023-04-24 22:21:42 -0500

    Add postcss-import & tailwindcss/nesting

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   plugins: {
+    // https://tailwindcss.com/docs/using-with-preprocessors
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
   },
