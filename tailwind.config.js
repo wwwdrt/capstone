802123a28f8aeefb617fd3ffa624784343407dea
juandapps <juandapps@hotmail.com>
2023-04-29 09:41:33 -0500

    Extend tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  content: [
-    "./pages/**/*.{html,js}",
-    "./components/**/*.{html,js}",
-  ],
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
@@ -18,20 +15,20 @@ module.exports = {
         place: "#787878",
         mainprogram: "#272a32",
         sponsors: "#414246",
-        'sponsors-font': "#e8e8e7",
+        "sponsors-font": "#e8e8e7",
         footer: "#2e2e2e",
         programbg: "rgba(60, 63, 70, 0.6)",
         gorange: "#f65d1a",
-
       },
       backgroundImage: {
-        'hero-pattern': "url(../capstone/public/bg-hero.png)",
-        'main-program': "url(../capstone/public/main-program/main-program-pattern.png)",
-        'chessboard': "url(../capstone/public/speakers/speaker-squares.png)",
+        "hero-pattern": "url(../capstone/public/bg-hero.png)",
+        "main-program":
+          "url(../capstone/public/main-program/main-program-pattern.png)",
+        chessboard: "url(../capstone/public/speakers/speaker-squares.png)",
       },
       screens: {
-        'xs': '300px',
-      }
+        xs: "300px",
+      },
     },
   },
   plugins: [],
